137a3cb5baffe51a9899d8e262d67b62ff59066f
Shiva Gopalkrishnan <shiva@shivas-macbook-pro.local>
2019-02-07 00:55:12 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Shiva Gopalkrishnan on 2/7/19.
+//  Copyright © 2019 Shiva Gopalkrishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
